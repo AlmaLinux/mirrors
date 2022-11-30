5302ffe5dc024ffdefd9c62aeedccf2f6debf686
soksanichenko <soksanichenko@cloudlinux.com>
2022-11-30 12:52:11 +0200

    ALBS-773: Speed up the hourly update of the mirrors list
    - Comments from review
    - Set geodata for expired mirrors
    - Tiny refactoring
    - Working User-agent

--- utils.py
+++ utils.py
@@ -29,14 +29,8 @@ from .data_models import (
 
 # set User-Agent for python-requests
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    "Upgrade-Insecure-Requests": "1",
-    "DNT": "1",
-    "Accept": "text/html,application/xhtml+xml,"
-              "application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate"
+    'User-Agent': 'curl/7.61.1',
+    'Accept': '*/*',
 }
 # the list of mirrors which should be always available
 WHITELIST_MIRRORS = (
