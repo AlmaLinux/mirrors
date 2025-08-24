4c1026f3abc5ca12743075292fb1682e6e768f90
Jonathan Wright <jonathan@almalinux.org>
2025-08-24 12:39:48 -0500

    Check timestamps on optional modules

--- src/backend/api/mirror_processor.py
+++ src/backend/api/mirror_processor.py
@@ -334,6 +334,10 @@ class MirrorProcessor:
         redis_key = f'mirror_offline_{mirror_info.name}'
         if (redis_value := self.cache.get(key=redis_key)) is not None:
             mirror_info.status = redis_value
+            self.logger.info(
+                'Mirror "%s" is marked as offline in Redis cache',
+                mirror_info.name
+            )
             return False
         if mirror_info.private or mirror_info.name in WHITELIST_MIRRORS:
             self.logger.info(
@@ -404,6 +408,20 @@ class MirrorProcessor:
         mirror_info.status = 'ok'
         
         for module in main_config.optional_module_versions.keys():
+            # check if the module is expired
+            # this first if condition is repetitive but that's ok
+            if not mirror_info.module_urls or not mirror_info.module_urls.get(module):
+                continue
+            if await self.is_mirror_expired(
+                    mirror_info=mirror_info,
+                    main_config=main_config,
+                    module=module
+            ):
+                self.logger.info(
+                    f'Mirror {mirror_info.name} module {module} is expired'
+                )
+                continue
+
             await optional_modules_available(
                 mirror_info=mirror_info,
                 http_session=self.client,
@@ -416,6 +434,7 @@ class MirrorProcessor:
         self,
         mirror_info: MirrorData,
         main_config: MainConfig,
+        module: Optional[str] = None
     ):
         mirror_should_updated_at = dateparser.parse(
             f'now-{main_config.allowed_outdate} UTC'
@@ -424,6 +443,7 @@ class MirrorProcessor:
             get_mirror_url(
                 main_config=main_config,
                 mirror_info=mirror_info,
+                module=module
             ) + '/',
             'TIME',
             )
